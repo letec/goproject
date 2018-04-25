36f59e06d0825abcdf19475e1ca9177215d5f097
Danny <mixdran@gmail.com>
2018-04-25 09:12:04 +0800

    dev

--- src/model/mysql.go
+++ src/model/mysql.go
@@ -45,8 +45,34 @@ func MysqlConnect() {
 	}
 }
 
+func scanAllParams(rows *sql.Rows) (map[string]interface{}, error) {
+	// 取得所有列
+	columns, _ := rows.Columns()
+	// 计算列的数量
+	length := len(columns)
+	// 定义一个存放值的map
+	value := make([]interface{}, length)
+	// 定义一个存放指针的map
+	columnPointers := make([]interface{}, length)
+	// 循环把变量地址拿到 &代表地址
+	for i := 0; i < length; i++ {
+		columnPointers[i] = &value[i]
+	}
+	// 这个是最经典的 把一个interface的切片作为可变参数传进去scan 有多少列都在columnPointers里面了
+	rows.Scan(columnPointers...)
+	// 创建结果变量存返回值
+	result := make(map[string]interface{})
+	// 循环把结果放到result里面
+	for i := 0; i < length; i++ {
+		columnName := columns[i]
+		columnValue := columnPointers[i].(*interface{})
+		result[columnName] = string((*columnValue).([]uint8))
+	}
+	return result, nil
+}
+
 // GetRow 取得单条数据
-func GetRow(table string, userDesc []string, where map[string]string) (*sql.Rows, error) {
+func GetRow(table string, userDesc []string, where map[string]string) (map[string]interface{}, error) {
 	set := ""
 	length := len(userDesc)
 	if length > 0 {
@@ -80,7 +106,8 @@ func GetRow(table string, userDesc []string, where map[string]string) (*sql.Rows
 		return nil, err
 	}
 	rows.Next()
-	return rows, err
+	result, err := scanAllParams(rows)
+	return result, err
 }
 
 // GetRows 取得多条数据
--- src/model/user.go
+++ src/model/user.go
@@ -9,32 +9,10 @@ func GetUserInfoByID(userid string) (map[string]interface{}, error) {
 		"id": "=" + userid,
 	}
 	// 这个方法我封装的 拼接SQL查询到结果
-	rows, err := GetRow("user", userDesc, where)
+	userInfo, err := GetRow("user", userDesc, where)
 	// 报错了就返回空和错误
 	if err != nil {
 		return nil, err
 	}
-	// 取得所有列
-	columns, _ := rows.Columns()
-	// 计算列的数量
-	length := len(columns)
-	// 定义一个存放值的map
-	value := make([]interface{}, length)
-	// 定义一个存放指针的map
-	columnPointers := make([]interface{}, length)
-	// 循环把变量地址拿到 &代表地址
-	for i := 0; i < length; i++ {
-		columnPointers[i] = &value[i]
-	}
-	// 这个是最经典的 把一个interface的切片作为可变参数传进去scan 有多少列都在columnPointers里面了
-	rows.Scan(columnPointers...)
-	// 创建结果变量存返回值
-	result := make(map[string]interface{})
-	// 循环把结果放到result里面
-	for i := 0; i < length; i++ {
-		columnName := columns[i]
-		columnValue := columnPointers[i].(*interface{})
-		result[columnName] = string((*columnValue).([]uint8))
-	}
-	return result, nil
+	return userInfo, nil
 }
