814c6596ec5a971eb9c4fb73045fc5edf7defc73
Danny <mixdran@gmail.com>
2018-04-30 13:36:41 +0800

    dev

--- src/model/user.go
+++ src/model/user.go
@@ -2,15 +2,11 @@ package model
 
 // GetUserInfoByID 查询用户名
 func GetUserInfoByID(userid string) (map[string]interface{}, error) {
-	// 定义一个(切片)类型,类似于PHP一维数组
 	userDesc := []string{"id", "username", "password", "salt", "realname", "phone", "bankCode"}
-	// WHERE条件写进map
 	where := map[string]string{
 		"id": "=" + userid,
 	}
-	// 这个方法我封装的 拼接SQL查询到结果
 	userInfo, err := GetRow("user", userDesc, where)
-	// 报错了就返回空和错误
 	if err != nil {
 		return nil, err
 	}
@@ -19,15 +15,11 @@ func GetUserInfoByID(userid string) (map[string]interface{}, error) {
 
 // GetUserInfoByIDs 查询用户名
 func GetUserInfoByIDs(userid string) (map[int]map[string]interface{}, error) {
-	// 定义一个(切片)类型,类似于PHP一维数组
 	userDesc := []string{"id", "username", "realname", "phone", "bankCode"}
-	// WHERE条件写进map
 	where := map[string]string{
 		"id": "=" + userid,
 	}
-	// 这个方法我封装的 拼接SQL查询到结果
 	userInfo, err := GetRows("user", userDesc, where, 0)
-	// 报错了就返回空和错误
 	if err != nil {
 		return nil, err
 	}
