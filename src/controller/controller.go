c8a714cbdc37eea480f1f9e803e45a672787ec9a
Danny <mixdran@gmail.com>
2018-05-14 11:59:34 +0800

    dev

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,10 +1,15 @@
 package controller
 
 import (
+	"encoding/json"
 	"model"
+	"net/http"
 )
 
 // GetMaintenance 得到维护情况
-func GetMaintenance() (map[string]string, error) {
-	return model.GetMaintenance()
+func GetMaintenance(w http.ResponseWriter) bool {
+	result := model.GetMaintenance()
+	b, _ := json.Marshal(result)
+	w.Write(b)
+	return true
 }
