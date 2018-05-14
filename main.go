f79fed4090c8e9d8b89995091b5e39a97e07c12a
Danny <mixdran@gmail.com>
2018-05-14 12:02:38 +0800

    dev

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"common"
 	"model"
+	"net/http"
+	"os"
 	"router"
 )
+
+const sysLog = "log/sysLog.log"
+
+// startServer 运行服务器
+func startServer() {
+	config, err := common.LoadServerConfig()
+	if err != nil {
+		common.WriteLog(sysLog, "加载server配置文件失败,退出程序!")
+		os.Exit(0)
+	}
+	if !common.CheckParamsExist([]string{"port"}, config) {
+		common.WriteLog(sysLog, "server配置文件没有填写正确,退出程序!")
+		os.Exit(0)
+	}
+	port := config["port"]
+	err = http.ListenAndServe("www.xr.com:"+port, nil)
+	if err != nil {
+		common.WriteLog(sysLog, "监听"+port+"端口失败,退出程序!")
+		os.Exit(0)
+	}
+}
 
 func main() {
 	// 连接MYSQL
@@ -19,5 +43,5 @@ func main() {
 	router.InitRouter()
 
 	// 开启HTTP服务
-	StartServer()
+	startServer()
 }
